0d7a91bc2684da617316d76d89f7b96e80824932
FoxConN-M <victormenezes1121@gmail.com>
2024-11-06 17:39:54 -0300

    Iniciando Projeto

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_read_root_deve_retornar_OK_e_eu_gosto_de_pizza():
+    client = TestClient(app)  # arrange
+    response = client.get('/')  # act
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == 'Eu gosto de pizza'
